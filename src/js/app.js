dd05787473d1ec4dad3e4c74df1be38927d3e0b3
Illya <illya-komar@ukr.net>
2023-04-25 07:57:12 +0300

    Add slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,7 @@
+/* eslint-disable */
+
+"use strict";
+
 import "../scss/app.scss";
-
-/* Your JS Code goes here */
-
-/* Demo JS */
-//import "./demo.js";
 
 import "./landing.js";
--- src/js/landing.js
+++ src/js/landing.js
@@ -1,16 +1,41 @@
+/* eslint-disable */
+
+import Swiper, { Navigation } from "swiper";
+// import Swiper and modules styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+console.log("Landing...");
+
 $(window).on("load", function () {
   console.log("Load...");
 
   $(".statistics-blocks").slick({
+    //autoplay: true,
     slidesToShow: 4,
     arrows: false,
   });
+
+  const swiper = new Swiper(".swiper", {
+    modules: [Navigation],
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 4,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 });
 
-$("#slick-next").on("click", function (evt) {
+$("#slick-next").on("click", function () {
   $(".statistics-blocks").slick("slickNext");
 });
 
-$("#slick-prev").on("click", function (evt) {
+$("#slick-prev").on("click", function () {
   $(".statistics-blocks").slick("slickPrev");
+});
+
+$(document).ready(function () {
+  console.log("Ready...");
 });
